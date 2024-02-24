4f85542296add707f48ce5ceba2bd54eff6ce643
Kancharla T V V Kumar <147421515+KancharlaTeja24@users.noreply.github.com>
2024-02-25 00:41:43 +0530

    Update server.cjs

--- src/server/server.cjs
+++ src/server/server.cjs
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
   {
-    origin:[''],
+    origin:['https://mern-todo-dun-eta.vercel.app'],
     methods:['POST','GET'],
     credentials:true
   }
